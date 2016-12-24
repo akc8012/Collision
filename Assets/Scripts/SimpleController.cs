1abf5e6eda656ed1c6695514c202122f12830bdd
Andrew Colannino <akcolannino@email.neit.edu>
2016-12-24 16:01:47 -0500

    overhaul -- check z via two separate walls

--- Assets/Scripts/SimpleController.cs
+++ Assets/Scripts/SimpleController.cs
@@ -19,17 +19,24 @@ public class SimpleController : MonoBehaviour
 	void Update()
 	{
 		vel = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-
-		for (int i = 0; i < restrictAxis.Length; i++)
+		#region nonsense
+		/*for (int i = 0; i < restrictAxis.Length; i++)
 		{
 			if (restrictAxis[i])
 			{
 				if (restrictDirs[i] == Direction.Forward) vel[i] = vel[i] > 0 ? 0 : vel[i];
 				if (restrictDirs[i] == Direction.Backward) vel[i] = vel[i] < 0 ? 0 : vel[i];
 			}
-		}
-
+		}*/
+		#endregion
 		transform.position += vel * speed * Time.deltaTime;
+	}
+
+	public void SetZ(float z)
+	{
+		Vector3 newPos = transform.position;
+		newPos.z = z;
+		transform.position = newPos;
 	}
 
 	public void Restrict(int axis, bool doRestriction, Direction restrictDir)
@@ -42,11 +49,12 @@ public class SimpleController : MonoBehaviour
 	public float z { get { return transform.position.z; } }
 
 	public Vector3 Vel { get { return vel; } }
+	public Vector3 Extents { get { return col.bounds.extents; } }
 
-	public float GetX { get { return col.bounds.max.x + (speed * Time.deltaTime); } }
-	public float GetMinX { get { return col.bounds.min.x - (speed * Time.deltaTime); } }
-	public float GetZ { get { return col.bounds.max.z + (speed * Time.deltaTime); } }
-	public float GetMinZ { get { return col.bounds.min.z - (speed * Time.deltaTime); } }
+	public float GetX { get { return col.bounds.max.x; } }
+	public float GetMinX { get { return col.bounds.min.x; } }
+	public float GetZ { get { return col.bounds.max.z; } }
+	public float GetMinZ { get { return col.bounds.min.z; } }
 
 	public bool zRestricted { get { return restrictAxis[2]; } }
 	public bool xRestricted { get { return restrictAxis[0]; } }
---
+++ Assets/Scripts/WallZ.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallZ : MonoBehaviour
+{
+	SimpleController player;
+	[SerializeField] bool inFront;
+
+	void Start()
+	{
+		player = GameObject.FindWithTag("Player").GetComponent<SimpleController>();
+		Vector3 meep = transform.position;
+		meep.x = transform.position.x + transform.localScale.x/2;
+		GameObject.Find("Dude").transform.position = meep;
+	}
+	
+	void Update()
+	{
+		float min = transform.position.x - transform.localScale.x/2;
+		float max = transform.position.x + transform.localScale.x/2;
+
+		bool checkPos = false;
+
+		if (inFront)
+			checkPos = InFrontZ();
+		else
+			checkPos = BehindZ();
+
+		if (WithinX(min, max) && checkPos)        // function pointer for InFrontZ -- BehindZ
+		{
+			float setZ = 0;
+
+			if (inFront)
+				setZ = InFrontPosZ();
+			else
+				setZ = BehindPosZ();
+
+			player.SetZ(setZ);
+		}
+	}
+
+	bool WithinX(float min, float max)
+	{
+		return (player.GetX > min) && (player.GetMinX < max);
+	}
+
+	bool InFrontZ()
+	{
+		return (player.GetZ > transform.position.z && player.GetMinZ < transform.position.z);
+	}
+
+	bool BehindZ()
+	{
+		return (player.GetMinZ < transform.position.z && player.GetZ > transform.position.z);
+	}
+
+	float InFrontPosZ()
+	{
+		return transform.position.z - player.Extents.z;
+	}
+
+	float BehindPosZ()
+	{
+		return transform.position.z + player.Extents.z;
+	}
+}
